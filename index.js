7315ac93f4c8ad6d232169fc9f815af0ff79a630
Admolgit <zealous890@gmail.com>
2023-04-05 09:09:13 +0100

    commented

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const app = express();
 const user = require("./src/Routes/user");
 const product = require("./src/Routes/product");
 
+// Application middlewares
 app.use(helmet())
 app.use(morgan('dev'));
 app.use(express.json({ limit: "900mb" }));
@@ -23,6 +24,7 @@ app.use(cors());
 app.use("/user", user);
 app.use("/", product)
 
+// Creating database
 app.get("/createdb", (req, res) => {
   console.log("createdb");
   let sql = "CREATE DATABASE Authentications";
